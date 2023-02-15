6c7b8c8e0a2a30041176e667b5cea86262972043
Dhruv Saxena <dhruv162002@gmail.com>
2023-02-15 22:02:53 +0530

    Display slides

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import cv2
+import os
 
 # Variables
 # Similar to presentation, but need not be
 width, height = 1280, 720
-
+folderPath = "Presentation"
 
 # Camera setup
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
-while True:
-    succes, img = cap.read()
-    
+#List of Presentation Images
+pathImages = sorted(os.listdir(folderPath), key = len)
 
+# Change Slide Number
+imgNumber = 0
+
+while True:
+    success, img = cap.read()
+    pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
+    imgCurrent = cv2.imread(pathFullImage)
 
     cv2.imshow("Image", img)
+    cv2.imshow("Slide", imgCurrent)
+
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
