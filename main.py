cca3e69661ae9757fd0cae82e5738805b0baf252
Dhruv Saxena <dhruv162002@gmail.com>
2023-02-15 22:30:11 +0530

    Detect Fingers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from cvzone.HandTrackingModule import HandDetector
 
 # Variables
 # Similar to presentation, but need not be
@@ -16,11 +17,31 @@ pathImages = sorted(os.listdir(folderPath), key = len)
 
 # Change Slide Number
 imgNumber = 0
+hs, ws = 120, 210
+
+# Hand Detector
+detector = HandDetector(detectionCon = 0.85, maxHands = 1)
+
 
 while True:
     success, img = cap.read()
+    # Flip Image horizontally
+    img = cv2.flip(img, 1)
     pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
     imgCurrent = cv2.imread(pathFullImage)
+
+    hands, img = detector.findHands(img)
+
+    if hands:
+        hand = hands[0]
+        # Check fingers up
+        fingers = detector.fingersUp(hand)
+        print(fingers)
+
+    # Adding WebCam on slides
+    imgSmall = cv2.resize(img, (ws, hs))
+    h, w, _ = imgCurrent.shape
+    imgCurrent[0:hs, w-ws:w] = imgSmall
 
     cv2.imshow("Image", img)
     cv2.imshow("Slide", imgCurrent)
