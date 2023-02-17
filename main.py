feb3626bb606a92ab8264a51dd51ed1956064219
Dhruv Saxena <dhruv162002@gmail.com>
2023-02-17 23:59:47 +0530

    Complete Code

--- main.py
+++ main.py
@@ -1,72 +1,121 @@
+from cvzone.HandTrackingModule import HandDetector
 import cv2
 import os
-from cvzone.HandTrackingModule import HandDetector
+import numpy as np
 
-# Variables
-# Similar to presentation, but need not be
+# Parameters
 width, height = 1280, 720
+gestureThreshold = 300
 folderPath = "Presentation"
 
-# Camera setup
+# Camera Setup
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
-#List of Presentation Images
-pathImages = sorted(os.listdir(folderPath), key = len)
-
-# Change Slide Number
-imgNumber = 0
-hs, ws = 120, 210
-gestureThreshold = 300
-buttonPress = False
-buttonCounter = 0
-buttonDelay = 30
-
 # Hand Detector
-detector = HandDetector(detectionCon = 0.85, maxHands = 1)
+detectorHand = HandDetector(detectionCon=0.8, maxHands=1)
 
+# Variables
+imgList = []
+delay = 30
+buttonPressed = False
+counter = 0
+drawMode = False
+imgNumber = 0
+delayCounter = 0
+annotations = [[]]
+annotationNumber = -1
+annotationStart = False
+hs, ws = int(120 * 1), int(213 * 1)  # width and height of small image
+
+# Get list of presentation images
+pathImages = sorted(os.listdir(folderPath), key=len)
+print(pathImages)
 
 while True:
+    # Get image frame
     success, img = cap.read()
-    # Flip Image horizontally
     img = cv2.flip(img, 1)
     pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
     imgCurrent = cv2.imread(pathFullImage)
 
-    hands, img = detector.findHands(img)
+    # Find the hand and its landmarks
+    hands, img = detectorHand.findHands(img)  # with draw
+    # Draw Gesture Threshold line
     cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
 
-    if hands and buttonPress is False:
+    if hands and buttonPressed is False:  # If hand is detected
+
         hand = hands[0]
-        # Check fingers up
-        fingers = detector.fingersUp(hand)
-        cx, cy = hand['center']
-        # print(fingers)
+        cx, cy = hand["center"]
+        lmList = hand["lmList"]  # List of 21 Landmark points
+        fingers = detectorHand.fingersUp(hand)  # List of which fingers are up
 
-        if cy <= gestureThreshold: # If hand is at height of the face
+        # Constrain values for easier drawing
+        xVal = int(np.interp(lmList[8][0], [width // 2, width], [0, width]))
+        yVal = int(np.interp(lmList[8][1], [150, height-150], [0, height]))
+        indexFinger = xVal, yVal
+
+        if cy <= gestureThreshold:  # If hand is at the height of the face
             if fingers == [1, 0, 0, 0, 0]:
+                print("Left")
+                buttonPressed = True
                 if imgNumber > 0:
-                    buttonPress = True
                     imgNumber -= 1
+                    annotations = [[]]
+                    annotationNumber = -1
+                    annotationStart = False
             if fingers == [0, 0, 0, 0, 1]:
+                print("Right")
+                buttonPressed = True
                 if imgNumber < len(pathImages) - 1:
-                    buttonPress = True
                     imgNumber += 1
+                    annotations = [[]]
+                    annotationNumber = -1
+                    annotationStart = False
 
-    if buttonPress:
-        buttonCounter += 1
-        if buttonCounter > buttonDelay:
-            buttonCounter = 0
-            buttonPress = False
+        if fingers == [0, 1, 1, 0, 0]:
+            cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
 
-    # Adding WebCam on slides
+        if fingers == [0, 1, 0, 0, 0]:
+            if annotationStart is False:
+                annotationStart = True
+                annotationNumber += 1
+                annotations.append([])
+            print(annotationNumber)
+            annotations[annotationNumber].append(indexFinger)
+            cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+
+        else:
+            annotationStart = False
+
+        if fingers == [0, 1, 1, 1, 0]:
+            if annotations:
+                annotations.pop(-1)
+                annotationNumber -= 1
+                buttonPressed = True
+
+    else:
+        annotationStart = False
+
+    if buttonPressed:
+        counter += 1
+        if counter > delay:
+            counter = 0
+            buttonPressed = False
+
+    for i, annotation in enumerate(annotations):
+        for j in range(len(annotation)):
+            if j != 0:
+                cv2.line(imgCurrent, annotation[j - 1], annotation[j], (0, 0, 200), 12)
+
     imgSmall = cv2.resize(img, (ws, hs))
     h, w, _ = imgCurrent.shape
-    imgCurrent[0:hs, w-ws:w] = imgSmall
+    imgCurrent[0:hs, w - ws: w] = imgSmall
 
+    cv2.imshow("Slides", imgCurrent)
     cv2.imshow("Image", img)
-    cv2.imshow("Slide", imgCurrent)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
